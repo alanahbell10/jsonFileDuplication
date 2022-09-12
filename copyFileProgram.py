eb5c96c812c261d4f6c4e9af01f0156753a07959
alanahbell10 <63126323+alanahbell10@users.noreply.github.com>
2022-09-12 17:53:19 -0400

    Updated input questions

--- copyFileProgram.py
+++ copyFileProgram.py
@@ -7,7 +7,11 @@ firstName = 0
 newName = firstName
 prevName = newName
 count = int(input("How many times would you like to duplicate " + str(firstName) + ".json? "))
-
+name = str(input("Enter name: "))
+symbol = str(input("Enter symbol: "))
+traitType = str(input("Enter trait type: "))
+value = str(input("Enter value: "))
+desc = str(input("Enter description: "))
 
 
 src = os.path.join('/Users/lana/desktop/generator', str(firstName) + '.json')
@@ -27,9 +31,13 @@ for i in range(0, count):
 
     with open(os.path.join('/Users/lana/desktop/generator/assets', str(prevName) + '.json'), 'r+') as file:
         data = json.load(file)
-        data['name'] = "Number #" + str(newName + 1)
+        data['name'] = name + " #" + str(newName + 1)
         data['image'] = str(newName) + ".png"
         data['properties']['files'][0]['uri'] = str(newName) + ".png"
+        data['attributes'][0]['trait_type'] = traitType
+        data['attributes'][0]['value'] = value
+        data['symbol'] = symbol
+        data['description'] = desc
         file.seek(0)
         json.dump(data, file, indent=2)
         file.truncate()
